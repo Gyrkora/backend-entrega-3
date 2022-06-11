8215c0ebfc5a11c4f9dd7b920b5871f7391d0df7
Gyrkora <giselle.gallardo0210@gmail.com>
2022-06-11 20:27:50 -0300

    detalles

--- express/server.js
+++ express/server.js
@@ -1,13 +1,3 @@
-/* Explicación 
-
-Realizar un proyecto de servidor basado en node.js que utilice el módulo express e implemente los sgtes endpoints en el puerto 8080
-
-2) Incluir un archivo de texto 'productos.txt' y utilizar la clase Contenedor del desafío anterior para accder a los datos persistidos del servidor.
-
-Antes de iniciar el servidor, colocar en el archivo 'productos.txt' tres productos como en el ejemplo del desafío anterior.
-
- */
-
 import express from 'express'
 import { Contenedor } from './productos.js'
 import fs from 'fs'
@@ -29,14 +19,6 @@ app.get('/productos', async (req, res) => {
 })
 
 //b) Ruta get '/productoRandom' que devuelva un producto elegido al azar entre todos los productos disponibles
-
-// const prodRandom = () => {
-// 	let randomNum = Math.floor(Math.random() * db.length)
-// 	const getRandom = db[randomNum]
-// 	return getRandom
-// 	// return db.length
-// }
-
 app.get('/productoRandom', async (req, res) => {
 	const getRandom = await prods.getRandomProduct()
 	res.send(getRandom)
